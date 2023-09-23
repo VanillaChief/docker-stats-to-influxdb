cb7f52f503ff1af35ec75cd8b08870aaab3c95e4
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-09-24 01:00:01 +0300

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-# Download script and service
+# Download script and service from Github repository
 service_path="/etc/systemd/system/docker-stats-to-influxdb.service"
 script_path="/root/docker-stats-to-influxdb.sh"
 curl https://raw.githubusercontent.com/Lifailon/docker-stats-to-influxdb/rsa/docker-stats-to-influxdb.service > $service_path
@@ -6,6 +6,6 @@ curl https://raw.githubusercontent.com/Lifailon/docker-stats-to-influxdb/rsa/doc
 # Service launch
 systemctl daemon-reload
 systemctl enable docker-stats-to-influxdb.service
-systemctl start docker-stats-to-influxdb
+systemctl restart docker-stats-to-influxdb
 # Get status
 systemctl status docker-stats-to-influxdb
